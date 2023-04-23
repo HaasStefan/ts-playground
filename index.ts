fa26812b6f81a091667ed6da13875159c1f3a1f7
Stefan Haas <stefan.haas.privat@gmail.com>
2023-04-23 09:40:07 +0200

    lambda expression

--- index.ts
+++ index.ts
@@ -2,9 +2,11 @@ function add(a: number, b: number) {
   return a + b;
 }
 
-function isNumber(x: unknown): x is number {
-  return typeof x === "number";
-}
+// function isNumber(x: unknown): x is number {
+//   return typeof x === "number";
+// }
+
+const isNumber = (x: unknown): x is number => typeof x === "number";
 
 const myArray = [1, 2, 3, 4, "5", 6];
 
